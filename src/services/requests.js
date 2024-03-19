e8146a7efcd789014fdb4c9b8138e224b731e160
GannaKov <zlatta2000@gmail.com>
2024-03-19 17:05:22 +0100

    change requests url

--- src/services/requests.js
+++ src/services/requests.js
@@ -5,12 +5,12 @@ const BASEURL = "http://localhost:3000";
 const instance = axios.create({ baseURL: BASEURL });
 
 export const getCategories = async () => {
-  const { data } = await instance.get();
+  const { data } = await instance.get("/animals");
   return data;
 };
 
 export const getLimitedAnimals = async () => {
-  const { data } = await instance.get("/animals");
+  const { data } = await instance.get("/main-page");
   console.log(data);
   return data;
 };
